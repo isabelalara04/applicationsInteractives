4ace0dfff8c1de0f1962fad3ff51d2c9d5060186
Isabela Lara <laramartinez.isabela@gmail.com>
2022-02-12 23:19:36 -0500

    ajute fichier gitignore

--- cleAPI.js
+++ cleAPI.js
@@ -1,2 +1,3 @@
 const cleAPI = "AAPK6a426526f0f946c38cb1050a9e8c109ctI91ZI1NO0RsIBLqbtEhBmzYGlQJpk8yHXDNzK8drUiFdmroJWWLSZ1Vhl1nD-Kj";
 
+const urlMeteo = "https://api.openweathermap.org/data/2.5/weather?q=Quebec&units=metric&lang=fr&appid=b80bebbe6af6a7f45ca0fda7dad54582";
